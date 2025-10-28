cc1a927be78c1cdd170ad08005a9ce6651aa228b
cub-alan <holwill.jacob@gmail.com>
2025-10-28 17:57:54 +0000

    f ksdlfvn sd

---
+++ nextState.m
@@ -0,0 +1,64 @@
+function nextState = TransitionFunction(state,action){
+
+north = 1;
+east = 2;
+south = 3;
+west = 4;
+
+tm(1,north) = 4;
+tm(1,east) = 1;
+tm(1,south) = 1;
+tm(1,west) = 1;
+
+tm(2,north) = 5;
+tm(2,east) = 2;
+tm(2,south) = 2;
+tm(2,west) = 2;
+
+tm(3,north) = 6;
+tm(3,east) = 3;
+tm(3,south) = 3;
+tm(3,west) = 3;
+
+tm(4,north) = ;
+tm(4,east) = ;
+tm(4,south) = ;
+tm(4,west) = ;
+
+tm(5,north) = ;
+tm(5,east) = ;
+tm(5,south) = ;
+tm(5,west) = ;
+
+tm(6,north) = ;
+tm(6,east) = ;
+tm(6,south) = ;
+tm(6,west) = ;
+
+tm(7,north) = ;
+tm(7,east) = ;
+tm(7,south) = ;
+tm(7,west) = ;
+
+tm(8,north) = ;
+tm(8,east) = ;
+tm(8,south) = ;
+tm(8,west) = ;
+
+tm(9,north) = ;
+tm(9,east) = ;
+tm(9,south) = ;
+tm(9,west) = ;
+
+tm(10,north) = ;
+tm(10,east) = ;
+tm(10,south) = ;
+tm(10,west) = ;
+
+tm(11,north) = ;
+tm(11,east) = ;
+tm(11,south) = ;
+tm(11,west) = ;
+
+
+}
